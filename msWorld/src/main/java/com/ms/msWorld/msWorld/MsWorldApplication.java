551020ffbee26a1429a636294177d967f9f7cbc7
Swapnil Chaudhari <swapnil.c17@gmail.com>
2018-10-30 17:08:35 +0530

    Added MS sample Project
    Added the code base to the repository

---
+++ msWorld/src/main/java/com/ms/msWorld/msWorld/MsWorldApplication.java
@@ -0,0 +1,12 @@
+package com.ms.msWorld.msWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsWorldApplication.class, args);
+	}
+}
